2823d838c055937ed9b18b5f06ba39cd2266d6bf
Lewis Okabe <lewizh11tmj@gmail.com>
2021-02-19 16:02:15 -0300

    delete .js in folder src

--- src/bot.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bot = void 0;
-var telegraf_1 = require("telegraf");
-var env_1 = __importDefault(require("./env"));
-exports.bot = new telegraf_1.Telegraf(env_1.default.TELEGRAM_API);
